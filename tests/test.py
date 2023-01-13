d5ec6899e4c34cc34598b4b480ee9cbfab386623
acse-ra922 <110524155+acse-ra922@users.noreply.github.com>
2023-01-13 10:07:12 +0000

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -15,3 +15,26 @@ result = 4
 
 def test_2():
     assert distance(string,params)==result
+
+s1 = "red green white violet red blue"
+param = "red, blue"
+r = 1
+
+def test_3():
+    assert distance(s1,param)==r
+
+
+s2 = "red red blue red"
+p = "red, blue"
+r1 = 1
+
+def test_4():
+    assert distance(s2,p)==r1
+
+
+s3 = "yellow yellow blue yellow yellow red"
+p3 = "red, blue"
+r3 = 3
+
+def test_5():
+    assert distance(s3,p3)==r3
